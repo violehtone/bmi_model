bc54de5bc11fdb765d03e4638738a4bcc74a5c4b
ville.x.lehtonen <ville.x.lehtonen@questdiagnostics.com>
2021-11-29 10:13:24 +0200

    fix linting errors, remove redundant --validate argument from model.py and update readme

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, Response
+from flask import Flask, request
 import pickle
 import pandas as pd
 import json
@@ -11,20 +11,22 @@ app = Flask(__name__)
 # Load the model
 model = pickle.load(open(MODEL_FILENAME, 'rb'))
 
+
 @app.route("/", methods=['GET'])
 def ping():
-    return "API is running!"
+    return "Ping!"
+
 
 @app.route("/bmi", methods=['POST'])
 def predict_bmi():
-    # 1) Read data from JSON to dict
+    # Read data from JSON to dict
     payload = request.get_json(force=True)
-    # 2) Convert dict to Pandas df and remove BMI and ID if they exist in the df
+    # Convert dict to Pandas df and remove 'BMI' and 'ID' columns
     df = pd.DataFrame.from_dict(payload, orient="index").T
     data = df.drop(labels=["BMI", "ID"], axis=1, errors='ignore')
-    # 4) predict BMI with the model
+    # predict BMI with the model
     prediction = model.predict(data)
-    # 5) Return prediction as json
+    # Return prediction as json
     return json.dumps(prediction.tolist())
 
 
--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from sklearn.experimental import enable_iterative_imputer # required for imputation to work
+from sklearn.experimental import enable_iterative_imputer # noqa F401 required for imputation to work
 from sklearn import linear_model, impute, metrics, model_selection
 import pandas as pd
 import numpy as np
@@ -14,18 +14,20 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 
-def save_model_on_disk(model, output_model_name):
+def save_model_on_disk(model: linear_model, output_model_name: str):
     """
     Saves the model on local disk
     """
     logger.info(f"Saving the model as: {output_model_name}...")
     pickle.dump(model, open(output_model_name, 'wb'))
-    logger.info(f"Done!")
+    logger.info("Done!")
 
 
-def preprocess_data(data: str, imputer: impute.IterativeImputer) -> pd.DataFrame:
+def preprocess_data(data: str, imputer: impute.IterativeImputer) \
+        -> pd.DataFrame:
     """
-    Reads the input data (.tsv file) into a Pandas dataframe and performs imputation in case of missing values
+    Reads the input data (.tsv file) into a Pandas dataframe and performs
+    imputation in case of missing values
     """
     logger.info(f"Reading {data} into a dataframe...")
     training_data = pd.read_csv(data, sep='\t')
@@ -37,7 +39,8 @@ def preprocess_data(data: str, imputer: impute.IterativeImputer) -> pd.DataFrame
     )
 
 
-def build_model(training_data: str, output_model_name: str, imputer: impute.IterativeImputer) -> linear_model:
+def build_model(training_data: str, output_model_name: str,
+                imputer: impute.IterativeImputer) -> linear_model:
     """
     Builds a Lasso regression model from the given training data
     """
@@ -45,9 +48,18 @@ def build_model(training_data: str, output_model_name: str, imputer: impute.Iter
     X = preprocessed_data.drop(labels=["BMI", "ID"], axis=1, errors='ignore')
     y = pd.DataFrame(preprocessed_data["BMI"]).values.ravel()
 
-    logger.info(f"Building a lasso regression model with 10-fold CrossValidation...")
-    cv = model_selection.RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
-    model = linear_model.LassoCV(alphas=np.arange(0.01, 1, 0.01), cv=cv, n_jobs=-1, tol=0.05)
+    logger.info("Building a lasso regression model with 10-fold CV")
+    cv = model_selection.RepeatedKFold(
+        n_splits=10,
+        n_repeats=3,
+        random_state=1
+    )
+    model = linear_model.LassoCV(
+        alphas=np.arange(0.01, 1, 0.01),
+        cv=cv,
+        n_jobs=-1,
+        tol=0.05
+    )
     model.fit(X, y)
 
     # Save model file on local disk
@@ -56,14 +68,25 @@ def build_model(training_data: str, output_model_name: str, imputer: impute.Iter
     return model
 
 
-def validate_model(test_data: str, imputer, model):
+def validate_model(test_data: str, imputer: impute.IterativeImputer,
+                   model: linear_model) -> None:
+    """
+    Validate the built model with a test data set.
+    :param test_data: .tsv file
+    :param imputer: imputer to replace missing values
+    :param model: linear model
+    """
     logger.info(f"Validating the model with {test_data}...")
     preprocessed_test_data = preprocess_data(data=test_data, imputer=imputer)
-    X = preprocessed_test_data.drop(labels=["BMI", "ID"], axis=1, errors='ignore')
+    X = preprocessed_test_data.drop(
+        labels=["BMI", "ID"],
+        axis=1,
+        errors='ignore'
+    )
     y = pd.DataFrame(preprocessed_test_data["BMI"]).values.ravel()
     prediction = model.predict(X)
-    
-    # Calculate accuracy of the predictions    
+
+    # Calculate accuracy of the predictions
     mse = metrics.mean_squared_error(
         y.tolist(),
         prediction
@@ -77,10 +100,10 @@ def validate_model(test_data: str, imputer, model):
     plt.title('Validation of the model on validation data')
     plt.xlabel('ID')
     plt.ylabel('BMI')
-    plt.savefig(f"plot.png")
+    plt.savefig("plot.png")
 
 
-def main(training_data: str, test_data: str, output_model_name: str, validate: bool):
+def main(training_data: str, test_data: str, output_model_name: str):
     # Define an imputer to be used for the datasets
     imputer = impute.IterativeImputer(max_iter=10, random_state=0, tol=0.01)
 
@@ -92,7 +115,7 @@ def main(training_data: str, test_data: str, output_model_name: str, validate: b
     )
 
     # Validate the model with the test data set
-    if validate:
+    if test_data:
         validate_model(
             test_data=test_data,
             imputer=imputer,
@@ -102,15 +125,16 @@ def main(training_data: str, test_data: str, output_model_name: str, validate: b
 
 def parse_args():
     """
-    Parses the input arguments. This model building script requires two .tsv files as its input. One for training the
-    lasso regression model and another one for optimizing it.
+    Parses the input arguments. This model building script requires two .tsv
+    files as its input. One for training the lasso regression model and
+    another one for optimizing it.
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('--training_data', type=str, required=True)
-    parser.add_argument('--test_data', type=str, required=True)
     parser.add_argument('--output_model_name', type=str, required=True)
-    parser.add_argument('--validate', action="store_true")
+    parser.add_argument('--test_data', type=str)
     return vars(parser.parse_args())
+
 
 if __name__ == '__main__':
     start_time = datetime.datetime.now()
@@ -120,4 +144,3 @@ if __name__ == '__main__':
     # Print the execution time of the script
     execution_time = datetime.datetime.now() - start_time
     logger.info(f"Execution time: {execution_time}")
-
